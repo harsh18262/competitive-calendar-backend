ffe553778fd377a01c228adddb687e77b6a63bcc
harsh18262 <harshwardhanmehrotra@gmail.com>
2021-04-26 15:49:37 +0530

    initial commit

---
+++ src/main/java/com/CodingCalendar/api/CodingCalendarBackendApplication.java
@@ -0,0 +1,13 @@
+package com.CodingCalendar.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingCalendarBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodingCalendarBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/CodingCalendar/api/CodingCalendarBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CodingCalendar.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodingCalendarBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
