883147d9466c740a95edfec24d87d4a75e5dd77f
harsh18262 <harshwardhanmehrotra@gmail.com>
2021-04-26 22:13:39 +0530

    beautified codechef scrapper's code

--- src/main/java/com/CodingCalendar/api/CodeChef/CodeChefDataScrapper.java
+++ src/main/java/com/CodingCalendar/api/CodeChef/CodeChefDataScrapper.java
@@ -1,7 +1,5 @@
 package com.CodingCalendar.api.CodeChef;
 
-
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
@@ -10,40 +8,31 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
-
-
-
 import com.CodingCalendar.api.WebDriverCreator;
 import com.CodingCalendar.api.entities.Contest;
 
 public class CodeChefDataScrapper {
-	
-	
-	
-		public List<Contest> Data() {
-			WebDriverCreator drivercreator=new WebDriverCreator();
-			WebDriver driver=drivercreator.CreateDriver();
-			
-			String url="https://codechef.com/contests";
-			driver.get(url);
-			
-			WebElement contests= driver.findElement(By.id("present-contests-data"));
-			List<WebElement> contest = contests.findElements(By.tagName("td"));
-			List<Contest> ContestList;
-			ContestList =new ArrayList<>();
-			for(int i=0;i<contest.size();i+=4)
-			{
-				ContestList.add(new Contest("CodeChef",contest.get(i+1).getText(),contest.get(i+2).getText(),contest.get(i+3).getText()));
-				
-			
-			}
-		    
-			driver.close();
-			
-			
-			
-			return ContestList;
-			
-		}
+
+  public List < Contest > Data() {
+    WebDriverCreator drivercreator = new WebDriverCreator();
+    WebDriver driver = drivercreator.CreateDriver();
+
+    String url = "https://codechef.com/contests";
+    driver.get(url);
+
+    WebElement contests = driver.findElement(By.id("present-contests-data"));
+    List < WebElement > contest = contests.findElements(By.tagName("td"));
+    List < Contest > ContestList;
+    ContestList = new ArrayList < > ();
+    for (int i = 0; i < contest.size(); i += 4) {
+      ContestList.add(new Contest("CodeChef", contest.get(i + 1).getText(), contest.get(i + 2).getText(), contest.get(i + 3).getText()));
+
+    }
+
+    driver.close();
+
+    return ContestList;
+
+  }
 
 }
