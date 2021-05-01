5311868c407683999b2b1858add4f6aefea1ad82
harsh18262 <harshwardhanmehrotra@gmail.com>
2021-05-02 02:08:53 +0530

    fixed the problem with CORS while accesing api from flutter

--- src/main/java/com/CodingCalendar/api/Controller/ApiController.java
+++ src/main/java/com/CodingCalendar/api/Controller/ApiController.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -19,6 +20,7 @@ import com.CodingCalendar.api.CodeForces.CodeForcesDataScrapper;
 
 
 @RestController
+@CrossOrigin(origins = "*")
 public class ApiController {
 	
 	@Autowired
